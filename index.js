258ab750e0ad04453025d8dd14f9d7428ebb3e0d
Caglar <caglarsulun@hotmail.com>
2021-12-01 18:13:39 -0500

    commit tree 85da859dbe4c9a26fa84b7604c63133ae8286ec4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//remove an element from the DOM
+const main = document.getElementById("main");
+main.remove();
+
+//creating element and then assigning its id and textcontent
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "DOM is the champion";
+
+// check the message on the console
+// DOM is the champion
+console.log(newHeader.textContent);
